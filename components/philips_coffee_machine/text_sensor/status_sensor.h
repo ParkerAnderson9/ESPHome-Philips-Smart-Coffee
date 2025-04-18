c42fa980421885e48005b3d9e493e0a4dcaf1726
Parker Anderson <118938802+ParkerAnderson9@users.noreply.github.com>
2025-04-18 18:15:11 -0500

    Update status_sensor.h

--- components/philips_coffee_machine/text_sensor/status_sensor.h
+++ components/philips_coffee_machine/text_sensor/status_sensor.h
@@ -8,7 +8,7 @@
 
 // Feel free to lower this, you might get some invalid intermittent state though
 #define REPEAT_REQUIREMENT 60
-#define BLINK_THRESHOLD 850
+#define BLINK_THRESHOLD 550
 
 namespace esphome
 {
